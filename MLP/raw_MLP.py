cf52ca6c2f688c35ac087bac95f95ab5db29c407
TLHY <54972550+TLHY@users.noreply.github.com>
2022-03-02 19:27:17 +0900

    Add files via upload

---
+++ MLP/raw_MLP.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 23 00:43:18 2021
+
+@author: tom49
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import os
+
+train_dir='./texture_data/train'
+    test_dir='./texture_data/test'
+    classes=['brick','grass','ground','water','wood']
+    
+
